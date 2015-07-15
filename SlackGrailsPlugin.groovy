2c785508f56f3be8dead6015201f6cb89c2b04e2
Mathi Fonseca <mathifonseca@gmail.com>
2015-07-15 12:30:45 -0300

    version 1.0.0

--- SlackGrailsPlugin.groovy
+++ SlackGrailsPlugin.groovy
@@ -1,5 +1,5 @@
 class SlackGrailsPlugin {
-    def version = "0.0.1"
+    def version = "1.0.0"
     def grailsVersion = "2.3 > *"
     def title = "Slack Plugin"
     def author = "Mathias Fonseca"
