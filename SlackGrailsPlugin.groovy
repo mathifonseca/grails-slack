43442fa00ea36d63ae53983955147ae1b9729069
Mathi Fonseca <mathifonseca@gmail.com>
2015-07-28 09:54:14 -0300

    changed documentation link

--- SlackGrailsPlugin.groovy
+++ SlackGrailsPlugin.groovy
@@ -1,11 +1,11 @@
 class SlackGrailsPlugin {
-    def version = "1.0.1"
+    def version = "1.0.2"
     def grailsVersion = "2.3 > *"
     def title = "Slack Plugin"
     def author = "Mathias Fonseca"
     def authorEmail = "mathifonseca@gmail.com"
     def description = "This plugin provides integration with Slack by Incoming Webhooks"
-    def documentation = "http://grails.org/plugin/slack"
+    def documentation = "https://github.com/mathifonseca/grails-slack"
     def license = "APACHE"
     def issueManagement = [ system: "GITHUB", url: "https://github.com/mathifonseca/grails-slack/issues" ]
     def scm = [ url: "https://github.com/mathifonseca/grails-slack" ]
