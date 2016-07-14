2198dc752518d416397ddc7131054871a2b06405
Helge Weissig <helgew@grajagan.org>
2016-07-14 14:16:39 -0700

    added footer support

--- src/main/groovy/grails/plugin/slack/SlackMessageAttachment.groovy
+++ src/main/groovy/grails/plugin/slack/SlackMessageAttachment.groovy
@@ -17,12 +17,15 @@ class SlackMessageAttachment implements Serializable, Validateable {
 	String text
 	String image_url
 	String thumb_url
+	String footer
+	String footer_icon
+	Integer ts
 	List<String> mrkdwn_in
 	List<SlackMessageAttachmentField> fields = []
 
 	static constraints = {
 		fallback nullable:true
-		color nullable:true, validator: { !it || it in ['good','warning','danger'] || it =~ /^#(?:[0-9a-fA-F]{3}){1,2}$/ }
+		color nullable:true, validator: { !it || it in ['good', 'warning', 'danger']|| it =~ /^#(?:[0-9a-fA-F]{3}){1,2}$/ }
 		pretext nullable:true
 		author_name nullable:true
 		author_link nullable:true, url:true
@@ -32,14 +35,24 @@ class SlackMessageAttachment implements Serializable, Validateable {
 		text nullable:true
 		image_url nullable:true, url:true
 		thumb_url nullable:true, url:true
-		mrkdwn_in nullable:true, validator: { list -> list.every { it in ['pretext','text','fields'] } }
+		mrkdwn_in nullable:true, validator: { list -> list.every { it in ['pretext', 'text', 'fields']} }
 		fields minSize:0
+		footer nullable: true
+		footer_icon nullable: true
+		ts nullable: true
 	}
 
-	 static {
-	     grails.converters.JSON.registerObjectMarshaller(SlackMessageAttachment) { that ->
-			 return that.class.declaredFields.findAll { that[it.name] && !java.lang.reflect.Modifier.isStatic(it.modifiers) && !it.synthetic && it.name != 'errors' && !it.name.startsWith('grails_') }.collectEntries { [ it.name, that[it.name]] }
-	     }
-	 }
-
+	static {
+		grails.converters.JSON.registerObjectMarshaller(SlackMessageAttachment) { that ->
+			return that.class.declaredFields.findAll {
+				that[it.name] &&
+				!java.lang.reflect.Modifier.isStatic(it.modifiers) &&
+				!it.synthetic &&
+				it.name != 'errors' &&
+				!it.name.startsWith('grails_')
+			}.collectEntries{
+				[it.name, that[it.name]]
+			}
+		}
+	}
 }
